f492629498212e88c942be32d81536dea0fcc3d3
Jericho FIllon <jerichofilloncoding@gmail.com>
2020-02-05 13:21:03 -0800

    Added MarkdownRemark to parse .md files and created new slugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,21 @@
  */
 
 // You can delete this file if you're not using it
+
+
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+//Dynamically creating a new slugs onto the MarkdownRemark nodes
+// -> this allows me to use the route to navigate to the route path that is declared.
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,54 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <h1>Jericho's Thoughts</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <span>
+            {node.frontmatter.title} - {node.frontmatter.date}
+          </span>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
 
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            description
+            title
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 // What I've done so far is:
 /*
-  -> Download a Markdown Cheatsheet
-  -> Update plugin in gatsby-config to "resolve" markdown pages
-  -> Shows up in GraphiQL
-  -> Ended it off there.
-  */ 
+  -> npm install gatsby-transformer-remark --S to parse Markdown files using Remark from Gatsby
+  -> using the query (graphql) imported from 'gatsby'
+  -> allMarkdownRemark parses the .md files from markdown-pages folder.
+  -> deconstruct the props into 'data'
+  -> display the data 
+  */
